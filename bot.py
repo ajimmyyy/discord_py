842dfd01467e8dbf0222f40402f6bef6316dcc66
ajimmyyy <80930376+ajimmyyy@users.noreply.github.com>
2022-03-30 23:39:53 +0800

    3/30

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import random
 import discord
 from discord.ext import commands
 import json
+import os
 
 with open('setting.json', mode = 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
@@ -24,14 +25,10 @@ async def on_member_remove(member):
     channel = bot.get_channel(int(jdata['channel']))
     await channel.send(f'{member} leave!')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'{round(bot.latency * 1000)} (ms)')
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
 
-@bot.command()
-async def koyo(ctx):
-    random_pic = random.choice(jdata['pic'])
-    koyori = discord.File(random_pic)
-    await ctx.send(file = koyori)
+if __name__ == "__main__":
+    bot.run(jdata['TOKEN'])
 
-bot.run(jdata['TOKEN'])
