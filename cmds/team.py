9b32f557abd2542945d554243508b9cb36cc7c35
ajimmyyy <80930376+ajimmyyy@users.noreply.github.com>
2022-04-11 15:07:44 +0800

    4/11_2

--- cmds/team.py
+++ cmds/team.py
@@ -1,4 +1,6 @@
 from multiprocessing.connection import wait
+from time import time
+import time
 from xmlrpc.client import DateTime
 import discord
 from discord.ext import commands
@@ -11,10 +13,9 @@ with open('setting.json', mode = 'r', encoding='utf8') as jfile:
 
 
 class Team(Cog_Extension):
-    #int(datetime.timedelta(minutes='5'))
-    start_time = int(datetime.datetime.now().strftime("%M")) - 6
-    counter = 1
     wait_time = 5
+    start_time = int(datetime.datetime.now().strftime("%M")) - wait_time - 1
+    counter = 2
 
     @commands.command()
     async def rand_squad_wait_time(self, ctx, msg):
@@ -50,7 +51,25 @@ class Team(Cog_Extension):
             self.team_counter = 0
         else:
             await ctx.send('劇情尚未結束')
+    
+    @commands.group()
+    async def atk(self, ctx):
+        pass
+    @atk.command()
+    async def sword(self, ctx):
+        await ctx.send("你的劍擦過對方的盔甲，你的劍產生些微裂痕")
+    @atk.command()
+    async def bow(self, ctx):
+        await ctx.send("你的箭筆直的飛向友軍，對方見你的低能行為，對你的戒心下降(抗性降低10%)")
+    @atk.command()
+    async def mag(self, ctx):
+        await ctx.send("小火球打在對方的盔甲上，但對方似乎有魔法抗姓")
+    @atk.command()
+    async def chicken(self, ctx):
+        await ctx.send("Chicken attack!\nWatch your back before it fades to black\nThey might look harmless but they’ll kick your non-chicken ass\nGo chicken go!\nGo chicken go!\nNow go, now fly\nYou own the sky")
+        time.sleep(2)
+        await ctx.send("什麼事都沒發生")
 
-
+    
 def setup(bot):
     bot.add_cog(Team(bot))
